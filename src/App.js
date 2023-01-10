d8b6d913ce2c60f637c30265768c76f82a9ed417
sebastianterleira <seb.terleira1204@gmail.com>
2023-01-10 13:12:52 -0500

    Se agrego AuthenticatedApp y UnauthenticatedApp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,31 @@
-import Search from './pages/search';
+import { useEffect, useState } from "react";
+
+import UnauthenticatedApp from "./UnauthenticatedApp";
+import AuthenticatedApp from "./AuthenticatedApp";
+import { getUser } from "./service/user-service";
+import { login, signUp } from "./service/auth-service";
+
 
 function App() {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    getUser().then(setUser).catch(console.log);
+  }, []);
+
+  console.log(user);
+
+  function handlelogin(credentials) {
+    login(credentials).then(setUser).catch(console.log);
+  }
+
+  function handleSignup(credentials) {
+    signUp(credentials).then(setUser).catch(console.log);
+  }
+  
   return (
     <>
-      <Search/>
+      { user ? <AuthenticatedApp/> : <UnauthenticatedApp onLogin={handlelogin} onSignup={handleSignup}/> }
     </>
   );
 }
