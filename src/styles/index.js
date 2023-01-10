824c42e3033975aafb4b4dbc097a097b13bdf3fe
sebastianterleira <seb.terleira1204@gmail.com>
2023-01-09 20:58:01 -0500

    Add: import Colors and styles

---
+++ src/styles/index.js
@@ -0,0 +1,5 @@
+import { colors } from "./colors";
+import { reset, global } from "./global";
+import { fonts, typography } from "./typography";
+
+export { colors, reset, fonts, typography, global };
