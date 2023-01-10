6c00c2af0e72fafc99994689191283436f40a1cd
sebastianterleira <seb.terleira1204@gmail.com>
2023-01-09 21:01:57 -0500

    Create Provisional Data until data can be fetched from the API

---
+++ src/data/data.js
@@ -0,0 +1,129 @@
+const data = [
+	{
+		id: 1,
+		category: 'Manufactoring',
+		image:
+			'https://samuelkraft.com/_next/image?url=%2Fblog%2Fvanilla-extract-with-next-themes%2Fimage.png&w=1080&q=75',
+		title: 'The job title',
+		company_name: 'The Company Name ',
+		type: 'Full time',
+		description: 'How to apply vanilla extract themes to Next.js apps',
+		min_max_price: '2.0k - 2.5k',
+		date: 'Published on Dec 17, 2021',
+		follow: true,
+	},
+	{
+		id: 2,
+		category: 'Manufactoring',
+		image:
+			'https://samuelkraft.com/_next/image?url=%2Fblog%2Fradix-ui-styling-with-css%2Fimage.png&w=1080&q=75',
+		title: 'The job title',
+		company_name: 'The Company Name ',
+		type: 'Full time',
+		description:
+			'Quick tip for styling Radix Primitives with plain CSS, CSS Modules, Vanilla Extract and more',
+		min_max_price: '2.0k - 2.5k',
+		date: 'Published on Dec 14, 2021',
+		follow: true,
+	},
+	{
+		id: 3,
+		category: 'Manufactoring',
+		image:
+			'https://samuelkraft.com/_next/image?url=%2Fblog%2Ffractional-svg-stars-css%2Fimage.png&w=1080&q=75',
+		title: 'The job title',
+		company_name: 'The Company Name ',
+		type: 'Full time',
+		description:
+			'Create a rating component with svg stars that support fractional values.',
+		min_max_price: '2.0k - 2.5k',
+		date: 'Published on Sep 06, 2021',
+		follow: true,
+	},
+	{
+		id: 4,
+		category: 'Manufactoring',
+		image:
+			'https://samuelkraft.com/_next/image?url=%2Fblog%2Fresponsive-animation-framer-motion%2Fimage.png&w=1080&q=75',
+		title: 'The job title',
+		company_name: 'The Company Name ',
+		type: 'Full time',
+		description:
+			'How to write responsive variants with media queries in javascript',
+		min_max_price: '2.0k - 2.5k',
+		date: 'Published on Jun 09, 2021',
+		follow: true,
+	},
+	{
+		id: 5,
+		category: 'Manufactoring',
+		image:
+			'https://samuelkraft.com/_next/image?url=%2Fblog%2Fbuilding-a-notion-blog-with-public-api%2Fimage.png&w=1080&q=75',
+		title: 'The job title',
+		company_name: 'The Company Name ',
+		type: 'Full time',
+		description:
+			'How to use Notion as a CMS using their public API and Next.js',
+		min_max_price: '2.0k - 2.5k',
+		date: 'Published on May 16, 2021',
+		follow: true,
+	},
+	{
+		id: 6,
+		category: 'Manufactoring',
+		image:
+			'https://samuelkraft.com/_next/image?url=%2Fblog%2Fmapbox-static-image-api%2Fimage.png&w=1920&q=75',
+		title: 'The job title',
+		company_name: 'The Company Name ',
+		type: 'Full time',
+		description:
+			'How to use Mapbox static image API and Node Canvas to generate custom sharing images',
+		min_max_price: '2.0k - 2.5k',
+		date: 'Published on May 09, 2021',
+		follow: true,
+	},
+	{
+		id: 7,
+		category: 'Manufactoring',
+		image:
+			'https://samuelkraft.com/_next/image?url=%2Fblog%2Ftrailroutes%2Fimage.png&w=1920&q=75',
+		title: 'The job title',
+		company_name: 'The Company Name ',
+		type: 'Full time',
+		description:
+			'How I built my side project Trail Routes',
+		min_max_price: '2.0k - 2.5k',
+		date: 'Published on Apr 19, 2021',
+		follow: true,
+	},
+	{
+		id: 8,
+		category: 'Manufactoring',
+		image:
+			'https://samuelkraft.com/_next/image?url=%2Fblog%2Fspring-parallax-framer-motion-guide%2Fbg.png&w=828&q=75',
+		title: 'The job title',
+		company_name: 'The Company Name ',
+		type: 'Full time',
+		description:
+			'A guide on creating a spring-based Parallax scrolling effect using Framer Motion and React',
+		min_max_price: '2.0k - 2.5k',
+		date: 'Published on Mar 15, 2021 ',
+		follow: true,
+	},
+	{
+		id: 9,
+		category: 'Manufactoring',
+		image:
+			'https://samuelkraft.com/_next/image?url=%2Fblog%2Fspring-parallax-framer-motion-guide%2Fbg.png&w=828&q=75',
+		title: 'The job title',
+		company_name: 'The Company Name ',
+		type: 'Full time',
+		description:
+			'A guide on creating a spring-based Parallax scrolling effect using Framer Motion and React',
+		min_max_price: '2.0k - 2.5k',
+		date: 'Published on Mar 15, 2021 ',
+		follow: true,
+	},
+];
+
+export default data;
