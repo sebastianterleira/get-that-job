f3654f103aa77e18cc68ff6209ee49798f44e347
Camilo-J <josecamilo1902@outlook.com>
2023-01-10 19:21:30 -0500

    Fix bug with styles

--- src/AuthenticatedApp.jsx
+++ src/AuthenticatedApp.jsx
@@ -1,17 +1,22 @@
+import styled from "@emotion/styled";
 import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/navbar";
 import Search from "./pages/search";
+
+const Container = styled.div`
+  display: flex;
+  height: 100vh;
+`;
 
 function AuthenticatedApp() {
   return (
-		<>
-		<Routes>
-			<Route 
-			path={"/home"}
-			element={<Search />}
-			/>
-		</Routes>
-		</>
-	);
+    <Container>
+      <Navbar />
+      <Routes>
+        <Route path={"/home"} element={<Search />} />
+      </Routes>
+    </Container>
+  );
 }
 
 export default AuthenticatedApp;
