f45cf0279777839900fd38ebfd5f384c998b532f
Camilo-J <josecamilo1902@outlook.com>
2023-01-11 08:37:09 -0500

    Update services for get-that-jo Api

--- src/context/auth-context.jsx
+++ src/context/auth-context.jsx
@@ -4,27 +4,33 @@ import * as auth from "../service/auth-services";
 
 const AuthContext = createContext();
 
-
 function AuthProvider(props) {
   const [user, setUser] = useState(null);
+  const [recruiter, setRecruiter] = useState(null);
 
-  useEffect(() => {
-    getUser().then(setUser).catch(console.log);
-  }, []);
-  
+  // useEffect(() => {
+  //   getUser().then(setUser).catch(console.log);
+  // }, []);
+
   function login(credentials) {
     auth.login(credentials).then(setUser).catch(console.log);
+  }
+
+  function lgoinRecruiter(credentials) {
+    auth.loginRecruiter(credentials).then(setRecruiter).catch(console.log);
   }
 
   function logout() {
     auth.logout().then(() => setUser(null));
   }
 
+  function logoutRecruiter() {
+    auth.logoutRecruiter().then(() => setRecruiter(null));
+  }
+
   function signup(userData) {
     createUser(userData).then(setUser).catch(console.log);
   }
-
-	console.log(user)
 
   const value = {
     user,
@@ -32,6 +38,9 @@ function AuthProvider(props) {
     login,
     logout,
     signup,
+    lgoinRecruiter,
+    recruiter,
+    logoutRecruiter,
   };
 
   return <AuthContext.Provider value={value} {...props} />;
