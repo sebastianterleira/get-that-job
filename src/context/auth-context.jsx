ca9cd694d11be9e066a3eb5414fb69b06574e1b2
Camilo-J <josecamilo1902@outlook.com>
2023-01-12 15:59:17 -0500

    Add method navigate

--- src/context/auth-context.jsx
+++ src/context/auth-context.jsx
@@ -1,14 +1,15 @@
 import { createContext, useContext, useEffect, useState } from "react";
-import { createUser, getRecruiter, getUser } from "../service/user-services";
+import { createUser, getRecruiter } from "../service/user-services";
 import * as auth from "../service/auth-services";
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const AuthContext = createContext();
 
 function AuthProvider(props) {
   const [user, setUser] = useState(null);
   const [recruiter, setRecruiter] = useState(null);
-
+  const navigate = useNavigate();
   // useEffect(() => {
   //   getUser().then(setUser).catch(console.log);
   // }, []);
@@ -45,6 +46,7 @@ function AuthProvider(props) {
     signup,
     lgoinRecruiter,
     recruiter,
+    navigate,
     logoutRecruiter,
   };
 
