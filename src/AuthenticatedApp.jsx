73414cebf6688d42bd3a44ebba5311f610ccfe54
Camilo-J <josecamilo1902@outlook.com>
2023-01-12 11:37:26 -0500

    Add routes for job & jobs page

--- src/AuthenticatedApp.jsx
+++ src/AuthenticatedApp.jsx
@@ -2,8 +2,10 @@ import styled from "@emotion/styled";
 import { Route, Routes } from "react-router-dom";
 import Navbar from "./components/navbar";
 import { useAuth } from "./context/auth-context";
+import JobPage from "./pages/jobPage";
 import ProfileRecruiter from "./pages/profilePage";
 import Search from "./pages/search";
+import JobsPage from "./pages/jobsPage";
 
 const Container = styled.div`
   display: flex;
@@ -23,6 +25,8 @@ function AuthenticatedApp() {
           <Routes>
             <Route path={"/home"} element={<Search />} />
             <Route path={"/profile"} element={<ProfileRecruiter />} />
+            <Route path={"/jobs"} element={<JobsPage />} />
+            <Route path={"/jobs/:id"} element={<JobPage />} />
           </Routes>
         </>
       )}
