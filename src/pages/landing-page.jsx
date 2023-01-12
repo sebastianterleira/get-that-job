ea994b232f90ee9f2acb2257f7381361a38ec07f
sebastianterleira <seb.terleira1204@gmail.com>
2023-01-12 06:18:25 -0500

    For now my Home page renders my Stepper template

--- src/pages/landing-page.jsx
+++ src/pages/landing-page.jsx
@@ -1,11 +1,15 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
 import styled from "@emotion/styled";
+import CustomizedSteppers from "../components/MaterialUI/StepperUI";
 import { fonts } from "../styles";
 
 function Home() {
   return (
+	<>
 		<h1>Home...</h1>
+		<CustomizedSteppers/>
+	</>
 	);
 }
 
