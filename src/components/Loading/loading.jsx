5982f99b7616044f3fc001fc26a622c2538aa9c2
sebastianterleira <seb.terleira1204@gmail.com>
2023-01-14 17:21:33 -0500

    Tempalte Loading

--- src/components/Loading/loading.jsx
+++ src/components/Loading/loading.jsx
@@ -1,14 +1,15 @@
 /** @jsxImportSource @emotion/react */
 import { css } from "@emotion/react";
+import React from "react"
 import styled from "@emotion/styled";
 import { Spinner } from "reactstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function Loading() {
   return (
-    <>
-      <h1>Hola....</h1>
-    </>
+    <div>
+      <Spinner color="primary"/>
+    </div>
   );
 }
 
