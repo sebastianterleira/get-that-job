612f3facf4da4b559da56f39d64031d8e924ff97
sebastianterleira <seb.terleira1204@gmail.com>
2023-01-14 17:18:16 -0500

    Add routes in AuthenticatedApp and UnauthenticatedApp

--- src/UnauthenticatedApp.jsx
+++ src/UnauthenticatedApp.jsx
@@ -1,9 +1,27 @@
 import Header from "./components/header";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/landing-page";
+import LoginPage from "./pages/login-page";
+import SignUpPage from "./pages/signup-page";
 
 function UnauthenticatedApp() {
   return (
 		<>
-			<Header/>
+		<Header/>
+			<Routes>
+					<Route
+					path={"/"}
+					element={<Home/>}
+					/>
+					<Route
+					path={"login"}
+					element={<LoginPage/>}
+					/>
+					<Route
+					path={"signup"}
+					element={<SignUpPage/>}
+					/>
+			</Routes>
 		</>
 	);
 }
